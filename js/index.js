70afa6949eebe0b5eae5f2e87be393ec5ea6dd09
RajuShetty <rajushetty.b@gmail.com>
2017-10-20 01:52:32 +0530

    build failed

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
     // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
+ document.addEventListener('deviceready', function () {
   // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+ window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
