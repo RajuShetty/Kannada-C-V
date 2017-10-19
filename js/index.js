e55c2a8bd18ed34753268b3045a2c75c77b6a130
RajuShetty <rajushetty.b@gmail.com>
2017-10-20 02:02:17 +0530

    build failed

--- js/index.js
+++ js/index.js
@@ -3,26 +3,65 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
-    // Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
- document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-  window.plugins.OneSignal
-    .startInit("d0aff88c-442d-44d5-b986-e98c32bd9513")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
+        console.log('Received Event: ' + id);
+        
+        // Enable to debug issues.
+        // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+        var iosSettings = {};
+        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
+        iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+
+        window.plugins.OneSignal.startInit( "d0aff88c-442d-44d5-b986-e98c32bd9513", "639924066309")
+                                .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
+                                .handleNotificationOpened(didOpenRemoteNotificationCallBack)
+                                .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+                                .iOSSettings(iosSettings)
+                                .endInit();
+    }
+};
+
+function didReceiveRemoteNotificationCallBack(jsonData) {
+        alert("Notification received:\n" + JSON.stringify(jsonData));
+        console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+    }
+function didOpenRemoteNotificationCallBack (jsonData) {
+        alert("Notification opened:\n" + JSON.stringify(jsonData));
+        console.log('didOpenRemoteNotificationCallBack: ' + JSON.stringify(jsonData));   
+    }
+
+function sendTag() {
+    window.plugins.OneSignal.sendTag("PhoneGapKey", "PhoneGapValue");
+}
+function getIds() {
+    window.plugins.OneSignal.getIds(function(ids) {
+        document.getElementById("OneSignalUserId").innerHTML = "UserId: " + ids.userId;
+        document.getElementById("OneSignalPushToken").innerHTML = "PushToken: " + ids.pushToken;
+        console.log('getIds: ' + JSON.stringify(ids));
+    });
 }
 
 app.initialize();
