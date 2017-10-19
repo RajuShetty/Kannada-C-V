b117abd192193daf43928518fedbc22ef0358787
RajuShetty <rajushetty.b@gmail.com>
2017-10-20 01:56:14 +0530

    build failed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
     // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
  document.addEventListener('deviceready', function () {
   // Enable to debug issues.
- window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
@@ -19,3 +23,6 @@
   // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
   // window.plugins.OneSignal.syncHashedEmail(userEmail);
 }, false);
+}
+
+app.initialize();
