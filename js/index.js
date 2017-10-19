817592c8a3a2f7b80e4741090a6515035c20a583
RajuShetty <rajushetty.b@gmail.com>
2017-10-20 01:11:01 +0530

    one signal push added

--- js/index.js
+++ js/index.js
@@ -1,90 +1,21 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById("toggleBtn").addEventListener('click', this.toggle, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-      console.log('deviceready event');
-      app.push = PushNotification.init({
-           "android": {
-               "senderID": "639924066309"
-           },
-           "ios": {
-             "sound": true,
-             "vibration": true,
-             "badge": true
-           },
-           "windows": {}
-       });
 
-       app.push.on('registration', function(data) {
-           console.log("registration event: " + data.registrationId);
-           document.getElementById("regId").innerHTML = data.registrationId;
-           var oldRegId = localStorage.getItem('registrationId');
-           if (oldRegId !== data.registrationId) {
-               // Save new registration ID
-               localStorage.setItem('registrationId', data.registrationId);
-               // Post registrationId to your app server as the value has changed
-           }
-       });
+    // Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
 
-       app.push.on('error', function(e) {
-           console.log("push error = " + e.message);
-       });
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
 
-       app.push.on('notification', function(data) {
-            console.log('notification event');
-            var cards = document.getElementById("cards");
-            var push = '<div class="row">' +
-              '<div class="col s12 m6">' +
-              '  <div class="card darken-1">' +
-              '    <div class="card-content black-text">' +
-              '      <span class="card-title black-text">' + data.title + '</span>' +
-              '      <p>' + data.message + '</p>' +
-              '      <p>' + data.additionalData.foreground + '</p>' +
-              '    </div>' +
-              '  </div>' +
-              ' </div>' +
-              '</div>';
-            cards.innerHTML += push;
-
-            app.push.finish(function() {
-                console.log('success');
-            }, function() {
-                console.log('error');
-            });
-        });
-    }
-};
-
-app.initialize();
+  window.plugins.OneSignal
+    .startInit("d0aff88c-442d-44d5-b986-e98c32bd9513")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
