ba541d727a109cd9125395392b4f5f8a199597d3
RajuShetty <rajushetty.b@gmail.com>
2017-10-24 21:29:10 +0530

    Admob added

--- js/admob.js
+++ js/admob.js
@@ -4,16 +4,16 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-    interstitial: 'ca-app-pub-6251426836814427/3272732399',
-    rewardvideo: 'ca-app-pub-6251426836814427/4960531197',
+    interstitial: 'ca-app-pub-6251426836814427/3248594733',
+    rewardvideo: 'ca-app-pub-6251426836814427/3242168952',
   };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+}  /*else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
     banner: '',
     interstitial: 'ca-app-pub-6251426836814427/7913997593',
     rewardvideo: 'ca-app-pub-6251426836814427/9390730792',
   };
-} /*else {
+}else {
   admobid = { // for Windows Phone
     banner: 'ca-app-pub-6869992474017983/8878394753',
     interstitial: 'ca-app-pub-6869992474017983/1355127956',
@@ -37,13 +37,13 @@ function initApp() {
   // this will load a full screen ad on startup
   AdMob.prepareInterstitial({
     adId: admobid.interstitial,
-   // isTesting: true, // TODO: remove this line when release
+    isTesting: true, // TODO: remove this line when release
     autoShow: true
   });
   
   prepareRewardVideoAd({
 	  adId: admobid.rewardvideo, 
-   // isTesting: true, // TODO: remove this line when release
+	isTesting: true, // TODO: remove this line when release
     autoShow: true });
 }
 
